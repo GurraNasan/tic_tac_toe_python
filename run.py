a6c5eab75bd14f3719039dfe27e32ef42665d272
GurraNasan <gurra.naslund@gmail.com>
2022-09-08 12:03:28 +0000

    Initial commit

--- run.py
+++ run.py
@@ -1,3 +1,11 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+def welcome():
+    print("")
+    print("####################################")
+    print("# Welcome to a game of TIC TAC TOE #")
+    print("####################################\n")
+    player_name = input("What is your name?: ")
+    
+
+
+welcome()
