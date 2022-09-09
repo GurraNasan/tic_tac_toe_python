0aedeefbe0703e1ffe15870dbc5b458801b97685
GurraNasan <gurra.naslund@gmail.com>
2022-09-09 08:58:02 +0000

    add rules and start option to welcome function

--- run.py
+++ run.py
@@ -1,10 +1,26 @@
 
+
 def welcome():
+    """
+    A function to welcome the user to the game, get the users name and tell the rules.
+    """
     print("")
     print("####################################")
     print("# Welcome to a game of TIC TAC TOE #")
     print("####################################\n")
     player_name = input("What is your name?: ")
+
+    print(f"\nOk, {player_name} the rules for this game is:")
+    print(
+    '''
+    The goal is to get a sequence from one side of the board to the other.
+    You will take turns with the computer. 
+    You can get a sequence horizontally, vertical or diagonally. 
+    If the board is full without anyone made a sequence it will be a tie  
+    ''')
+    start_answer = input("Ready to start press y for yes, y for no: ")
+    
+
     
 
 
