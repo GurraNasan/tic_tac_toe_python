d2e0809a0e16ddde001fff30c44aae8a114f8294
GurraNasan <gurra.naslund@gmail.com>
2022-09-09 12:58:30 +0000

    add ready to start function and made it check if user write y or n

--- run.py
+++ run.py
@@ -20,8 +20,9 @@ def welcome():
     You can get a sequence horizontally, vertical or diagonally. 
     If the board is full without anyone made a sequence it will be a tie  
     ''')
-    start_answer = input("Ready to start press y for yes, y for no: ")
-    
+    return player_name
+
+
 def get_player_name():
     """
     A function to get the players name and validate that it is only letters used. 
@@ -35,4 +36,24 @@ def get_player_name():
         else:
             print("You can only use letters, please try again")
 
+
+def ready_to_start():
+    """
+    A function to check if the user is ready to start the game
+    """
+    while True:
+
+        start_answer = input("Ready to start press y for yes, n for no: ").lower()
+
+        if start_answer == "y":
+            print("\nGame is starting")
+            break
+        elif start_answer == "n":
+            print("\nOk have a nice day")
+            break
+        else:
+            print("\nYou need to answer with a y or n\n")
+
+
 welcome()
+ready_to_start()
